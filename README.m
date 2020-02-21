56ec57b934761ebb30818a253e07a12a4a0429ba
amal <amal@calixa.co>
2020-02-21 00:01:33 -0500

    Add binder link

--- README.m
+++ README.m
@@ -1,3 +1,3 @@
 
 Click on this link to show an example of generating dynamic states of parcellations
-[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/SIMEXP/dynamic-states-parcellations.git/master?filepath=https%3A%2F%2Fgithub.com%2FSIMEXP%2Fdynamic-states-parcellations%2Fblob%2Fmaster%2Fnotebooks%2FGenerate_states_stability_maps_dynpar.ipynb)
+[![Binder](https://mybinder.org/badge_logo.svg)](https://hub.gke.mybinder.org/user/simexp-dynamic--s-parcellations-nhxm7t7x/notebooks/notebooks/Generate_states_stability_maps_dynpar.ipynb)
