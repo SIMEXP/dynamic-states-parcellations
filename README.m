1106c07801ddc5107d2ab56aeba6fac97e11a092
amal <amal@calixa.co>
2020-02-21 00:09:36 -0500

    Update setup.py

--- README.m
+++ README.m
@@ -1,3 +1,6 @@
 
 Click on this link to show an example of generating dynamic states of parcellations
-[![Binder](https://mybinder.org/badge_logo.svg)](https://hub.gke.mybinder.org/user/simexp-dynamic--s-parcellations-nhxm7t7x/notebooks/notebooks/Generate_states_stability_maps_dynpar.ipynb)
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/SIMEXP/dynamic-states-parcellations/980d28d9af72500c3beb7adecf609c27fe3522b0)
+
+
+
--- setup.py
+++ setup.py
@@ -11,9 +11,4 @@ setup(
     url='',
     packages=['dynpar'],
 
-    install_requires=[ 'seaborn','pytest', 'numpy', 'nilearn', 'nibabel', 'scipy',
-                      'sklearn', 'matplotlib', 'nose', "dask[array,dataframe]>=1.0.0",
-                      'psutil', 'bokeh', 'dask', 'joblib', 'scikit-image','pathos',
-                      'dash', 'dash_core_components', 'dash_html_components', 'SharedArray',
-                      'pandas_datareader', 'toolz', 'cytoolz', 'cachey', 'pyclustering', 'h5py', 'cufflinks'],
 )
